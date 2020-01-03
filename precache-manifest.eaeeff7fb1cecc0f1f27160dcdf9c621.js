91b9db54b16ec53b3bafde446c8a30bc1ce2ad36
my828 <minyang828@gmail.com>
2020-01-02 23:21:04 -0800

    Updates

---
+++ precache-manifest.eaeeff7fb1cecc0f1f27160dcdf9c621.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ea275cc8e67e435ea5ed3c3d3d8b20",
+    "url": "/helpfulhuman-challenge/index.html"
+  },
+  {
+    "revision": "9e18f2b54c6c28c6d7fc",
+    "url": "/helpfulhuman-challenge/static/css/main.ece8aba4.chunk.css"
+  },
+  {
+    "revision": "23d200f413e0f3aac12b",
+    "url": "/helpfulhuman-challenge/static/js/2.ae13858c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/helpfulhuman-challenge/static/js/2.ae13858c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9e18f2b54c6c28c6d7fc",
+    "url": "/helpfulhuman-challenge/static/js/main.476d61ee.chunk.js"
+  },
+  {
+    "revision": "39c4c66d2bf7ef169617",
+    "url": "/helpfulhuman-challenge/static/js/runtime-main.13b9d8ac.js"
+  },
+  {
+    "revision": "358d785ad976a211f49807c63694223b",
+    "url": "/helpfulhuman-challenge/static/media/logo-symbol.358d785a.svg"
+  }
+]);
