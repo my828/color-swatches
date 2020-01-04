7d9c722da066bdf29af204cc2ff6ddc374cb9724
my828 <minyang828@gmail.com>
2020-01-03 17:20:37 -0800

    edit readme

--- FONT.md
+++
@@ -1,4 +0,0 @@
-This project includes a font from Google Fonts.
-It's up to you how you'd like to implement it.
-
-https://fonts.google.com/specimen/Source+Serif+Pro
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,7 +5,7 @@ import Colors from '../constants/colors'
 const Navbar = () => {
     return (
         <nav style={styles.navbar}>
-            <a className="ml-3" href="httpw://my828.github.io/helpfulhuman-challenge">
+            <a className="ml-3" href="httpw://my828.github.io/color-swatches">
                 <img src={Logo} alt="logo" width="40" height="40" style={styles.logo}/>
             </a>
         </nav>
